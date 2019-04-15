6f19f8f2bb96d94d1a151d462aad04f39f882da6
zipang <christophe.desguez@gmail.com>
2019-04-15 19:59:45 +0200

    Docs + some simplifications to pass the navigation

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -7,31 +7,34 @@ import "./header-styles.less";
 import logo from "./logo.png";
 
 const extractPageName = (p) => p.split("/").pop();
-const buildNavigation = ({ entries = [] }, selected) => {
-	return [
-		...entries.map((nav) => (
-			<Link
-				to={nav.link}
-				role="menuitem"
-				tabIndex="0"
-				title={extractPageName(nav.link)}
-				className={nav.link === selected ? "selected" : undefined}
-			>
-				{nav.text}
-			</Link>
-		)),
-	];
-};
 
 /**
  *
- * @param {Object} header_navigation - contains an array of {link, text} entries
+ * @param {Array}  entries  - navigation entries (link + text)
+ * @param {String} selected - the navigation link that is currently selected
+ */
+const buildNavigation = ({ entries = [] }, selected) =>
+	entries.map((nav) => (
+		<Link
+			to={nav.link}
+			role="menuitem"
+			tabIndex="0"
+			title={extractPageName(nav.link)}
+			className={nav.link === selected ? "selected" : undefined}
+		>
+			{nav.text}
+		</Link>
+	));
+
+/**
+ *
+ * @param {Object} menu - contains an array of {link, text} entries
  * @param {Object} site_metadata - contains an array of {link, text} entries
  * @param {String} path   - the current page path (to select in the navigation menu)
  * @param {Boolean} fixed - to fix the position of the header when scrolling down
  */
 const Header = ({
-	header_navigation,
+	menu = {},
 	site_metadata: { site_title },
 	path,
 	fixed,
@@ -43,10 +46,10 @@ const Header = ({
 			</Link>
 		</div>
 
-		{header_navigation && (
+		{menu.entries && (
 			<HamburgerMenu
 				id="main-navigation"
-				navigationLinks={buildNavigation(header_navigation.entries, path)}
+				navigationLinks={buildNavigation(menu.entries, path)}
 			/>
 		)}
 	</header>
