5fd65df701f45507376002135f844a5673be099d
zipang <christophe.desguez@gmail.com>
2019-05-09 22:49:18 +0200

    progressing toward a more functional implementation

--- src/sections/GridBoard/index.js
+++ src/sections/GridBoard/index.js
@@ -1,25 +1,29 @@
 import React from "react";
+import { createKey } from "../../utils/ReactTools";
 
 import "./grid-board-styles.less";
 
+const h = React.createElement;
+
 /**
- * Add the `colWidth` and `rowHeight` props to a cell of the grid
+ * hoC : Add the `colWidth` and `rowHeight` props to a cell of the grid
  * @param {Number} cw - column width in percents of the grid width (0-100)
  * @param {Number} rh - row height in percents of the grid height (0-100)
  */
-const withGridDimensions = (cw, rh) => (Wrapped) => (...props) =>
-	<Wrapped colWidth={cw} rowHeight={rh} {...props} />;
+const withGridDimensions = (cw, rh) => (Wrapped, i) => (...props) =>
+	<Wrapped colWidth={cw} rowHeight={rh} key={createKey("cell-",i)} {...props} />;
 
 /**
- * Define a grid of cols x rows cells
- * Then GridBoard.Cells spanning multiples rows or columns
- * can be absolutely positionned inside the grid,
+ * Define a grid of `cols` x `rows` cells
+ *
+ * GridBoard.Cells spanning multiples rows or columns
+ * can be absolutely positionned inside this grid,
  *
  * @param {Number} cols - number of columns
  * @param {Number} rows - number of rows
- * @param {Boolean} hasContainer [false] - add a container around the children
- * @param {String} containerClassName ["container"] - container class name
- * @param {Array[GridBoard.Cells]} children
+ * @param {Boolean} [hasContainer=false] - add a container around the children
+ * @param {String}  [containerClassName="container"] - container class name
+ * @param {GridBoard.Cells[]} children
  */
 const GridBoard = ({
 	cols = 5,
@@ -27,37 +31,55 @@ const GridBoard = ({
 	hasContainer = false,
 	containerClassName = "container",
 	children,
+	cells = [],
 	...props
 }) => {
+
 	const colWidth  = 100 / cols;
 	const rowHeight = 100 / rows;
-	const cells = children.map(withGridDimensions(colWidth, rowHeight));
 
-	return (
-		<section className="grid-board" {...props}>
-			{ hasContainer ?
-				<div className={containerClassName}>{ cells }</div> :
-				{ cells }
-			}
-		</section>
-	)
+	const gridCells =
+		children ?
+			children.map(withGridDimensions(colWidth, rowHeight)) :
+			cells.map(withGridDimensions(colWidth, rowHeight)).map(GridBoard.Cell);
+
+	const gbSection = h(
+		"section",
+		{ className: "grid-board", ...props },
+		hasContainer ?
+			h("div", {className:containerClassName}, gridCells) :
+			gridCells
+	);
+
+	return gbSection;
 }
 
-GridBoard.Cells = ({
+/**
+ * Child elements of a grid board
+ * They have a position [col, row] inside the grid
+ * And a width and height (colSpan, rowSpan)
+ * expressed as the number of cols and rows that they cover
+ * @param {Number} col - column index (1-based)
+ * @param {Number} row - row index (1-based)
+ * @param {Number} [colSpan=1]
+ * @param {Number} [rowSpan=1]
+ */
+GridBoard.Cell = ({
 	col,
 	row,
-	colWidth,
-	rowHeight,
 	colSpan = 1,
 	rowSpan = 1,
+	colWidth,
+	rowHeight,
 	children,
 	className = '',
 	...props
 }) => {
 
 	className += " cell";
+
 	const cellStyle = {
-		position: "absolute";
+		position: "absolute",
 		top: ((row-1) * rowHeight) + "%",
 		left: ((col-1) * colWidth) + "%",
 		width: (colWidth * colSpan) + "%",
