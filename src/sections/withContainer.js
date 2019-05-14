c099ffa73ed66fb6974fea1e35d2101df9565d51
zipang <christophe.desguez@gmail.com>
2019-05-14 15:01:30 +0200

    Mise en forme

--- src/sections/withContainer.js
+++ src/sections/withContainer.js
@@ -1,19 +1,37 @@
-
-const withContainer = (Section) => ({
-	hasContainer = true,
-	containerClassName = "container",
-	...props,
-	children }) => {
+/**
+ * Surround the children of a component (generally a section)
+ * with a container (like in Bootstrap)
+ * The use case is to apply a responsive width
+ * @param {Function|Object} Section
+ */
+const withContainer = (Section) =>
+	/**
+	 * Define and intercept required params
+	 * @param {Boolean} [hasContainer=false] - add a container around the children
+	 * @param {String}  [containerClassName="container"] - container class name
+	 */
+	({
+		hasContainer = true,
+		containerClassName = "container",
+		...props,
+		children
+	}) => {
 		if (hasContainer) {
 			return (
 				<Section {...props}>
 					<div className={containerClassName}>
-						(children)
+						{children}
 					</div>
 				</Section>
-			)
+			);
+
+		} else {
+			return (
+				<Section {...props}>
+					{children}
+				</Section>
+			);
 		}
-		return <Section {...props}>{children}</Section>
 	};
 
 export default withContainer;
