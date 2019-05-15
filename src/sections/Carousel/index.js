e30419f519bf309a7900b623a1a6e4f24b3c94af
zipang <christophe.desguez@gmail.com>
2019-05-15 21:51:02 +0200

    Fixed carousel section container's height

--- src/sections/Carousel/index.js
+++ src/sections/Carousel/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Swiper from "react-id-swiper";
 import ReactMarkdown from "react-markdown";
+import { createKey } from "../../utils/ReactTools";
 
 import "../../../node_modules/react-id-swiper/src/styles/css/swiper.css";
 
 import "./carousel-styles.less";
-
-const slideId = (sectionId, i) =>
-	`${sectionId}-slide-${(100 + i).toString().substr(1)}`;
 
 /**
  * A carousel section based on Swiper
@@ -16,10 +14,13 @@ const slideId = (sectionId, i) =>
  */
 const Carousel = ({ id, title, slides }) => (
 	<section id={id} className="carousel" section_name="carousel">
-		<Swiper lazy={true} loop={true} initialSlide={1}>
+		<Swiper loop={true} initialSlide={1}>
 			{slides.map(({ image, text }, i) => (
-				<div className="slide" key={slideId(id, i)}>
-					<img className="swiper-lazy" src={image} alt={text} />
+				<div className="slide"
+					key={createKey("slide-", i)}
+					style={{
+						backgroundImage: `url('${image}')`
+					}}>
 
 					{text && (
 						<div className="text">
